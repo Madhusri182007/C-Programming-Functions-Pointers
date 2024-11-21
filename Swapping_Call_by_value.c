b7a05cf63ddca83a391b76ce4934858e0c86920f
Madhusri182007 <madhusri182007@gmail.com>
2024-11-21 20:19:12 +0530

    Create Swapping_Call_by_value.c

---
+++ Swapping_Call_by_value.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void swap(int a,int b)
+{
+    int t;
+    t=a;
+    a=b;
+    b=t;
+    printf("After swapping in func:%d %d\n",a,b);
+}
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("Before swapping in main:%d %d\n",a,b);
+    swap(a,b);
+    printf("After swapping in main:%d %d\n",a,b);
+}
