35cc04df8b68a1d08927ad86761794334cdd52cc
Madhusri182007 <madhusri182007@gmail.com>
2024-11-25 20:53:24 +0530

    Create Array2D_Pointer_Basic.c

---
+++ Array2D_Pointer_Basic.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a[2][2]={{1,2},{3,4}};
+    printf("%d ",*(*(a+0)+0));
+    printf("%d ",*(*(a+0)+1));
+    printf("%d ",*(*(a+1)+0));
+    printf("%d ",*(*(a+1)+1));
+}
